e6d3cb6ed4a674329082a982527d2b82991a7ac1
Nicole <nicolegonzaleslearn@gmail.com>
2026-04-18 21:54:57 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,13 +1,39 @@
-from flask import jsonify
-from service.routes import app
-from service.common import status
+from flask import jsonify, request, abort
+from service.models import Account
+from service.common import status  # Importante para los códigos de estado
+from . import app # Importa la instancia de Flask creada en __init__.py
 
+######################################################################
+# GET INDEX
+######################################################################
 @app.route("/", methods=["GET"])
 def index():
-    """Página de inicio"""
-    return jsonify(name="Account Rest API Service", version="1.0"), status.HTTP_200_OK
+    """Root URL response"""
+    return jsonify(
+        name="Account Rest API Service",
+        version="1.0",
+        paths=url_for("list_accounts", _external=True),
+    ), status.HTTP_200_OK
 
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """Listar todas las cuentas"""
-    return jsonify([]), status.HTTP_200_OK
+    app.logger.info("Request to list Accounts")
+    accounts = Account.all()
+    results = [account.serialize() for account in accounts]
+    return jsonify(results), status.HTTP_200_OK
+
+######################################################################
+# READ AN ACCOUNT
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """Leer una cuenta específica"""
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
