ff4a34e53bb896f8553c5ad1fae3b8e445ec6333
Nicole <nicolegonzaleslearn@gmail.com>
2026-05-09 17:48:52 -0400

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -23,13 +23,14 @@ def create_app():
             try:
                 models.init_db(app) # <--- Ahora sí funcionará
                 app.logger.info("Database initialized!")
-            except Exception as error:
-                app.logger.error(f"Database error: {error}")
                 
-                # Esto es lo que mataba tus tests:
-                if app.config.get("ENV") == "production" and "pytest" not in sys.modules:
-                    sys.exit(4)
-
+            except Exception as error:
+        app.logger.error(f"Database error: {error}")
+        # Solo cerramos si es producción REAL y NO estamos en un test
+        if app.config.get("ENV") == "production" and "pytest" not in sys.modules:
+            sys.exit(4)
+        else:
+            app.logger.info("Continuando a pesar del error de DB (Ambiente de Test/Desarrollo)")
     return app
 if __name__ == "__main__" or "gunicorn" in sys.argv[0]:
     app = create_app()
