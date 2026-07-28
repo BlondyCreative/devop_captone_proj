458e2233444400ff4774612da29b928a2271ecb1
Nicole Rodriguez <nicolegonzaleslearn@gmail.com>
2026-07-28 10:06:14 -0400

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,9 +1,29 @@
 from flask import Flask
+from flask_cors import CORS
+from service.models import db, init_db
+from service.routes import bp
+
 
 def create_app():
     app = Flask(__name__)
 
-    from .routes import bp
+    # Configuración de la base de datos
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounts.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Inicializar SQLAlchemy
+    db.init_app(app)
+
+    # Crear tablas
+    init_db(app)
+
+    # CORS
+    CORS(app)
+
+    # Registrar rutas (Blueprint)
     app.register_blueprint(bp)
 
     return app
+
+
+app = create_app()
