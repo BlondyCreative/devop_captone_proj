7edda97c9e826b5ff2948d6539ef153594924d95
Nicole Rodriguez <nicolegonzaleslearn@gmail.com>
2026-07-28 12:11:40 -0400

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,29 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
-from service.models import db, init_db
-from service.routes import bp
-
+from .models import db, init_db
 
 def create_app():
     app = Flask(__name__)
-
-    # Configuración de la base de datos
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounts.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    # Inicializar SQLAlchemy
-    db.init_app(app)
-
-    # Crear tablas
-    init_db(app)
-
-    # CORS
     CORS(app)
 
-    # Registrar rutas (Blueprint)
+    # Configuración de la base de datos
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/accounts.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    init_db(app)
+
+    from .routes import bp
     app.register_blueprint(bp)
 
     return app
 
-
+# ESTA LÍNEA ES CLAVE PARA LOS TESTS
 app = create_app()
