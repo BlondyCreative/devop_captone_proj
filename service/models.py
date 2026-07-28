d82d84d613aaf298a83d66922529124edb6e794b
Nicole Rodriguez <nicolegonzaleslearn@gmail.com>
2026-07-28 13:47:49 -0400

    Update models.py

--- service/models.py
+++ service/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
+
 db = SQLAlchemy()
+
 
 def init_db(app):
     """Inicializa la base de datos y crea tablas si no existen."""
     with app.app_context():
         db.create_all()
+
 
 class Account(db.Model):
     __tablename__ = "accounts"
@@ -18,28 +21,28 @@ class Account(db.Model):
     phone_number = db.Column(db.String(32), nullable=True)
     date_joined = db.Column(db.Date(), nullable=False, default=date.today)
 
-    # CREATE
     def create(self):
+        """CREATE: guarda la instancia en la base de datos."""
         db.session.add(self)
         db.session.commit()
         return self
 
-    # READ
     @staticmethod
     def find(account_id):
+        """READ: busca una cuenta por id."""
         return Account.query.get(account_id)
 
-    # LIST
     @staticmethod
     def all():
+        """LIST: devuelve todas las cuentas."""
         return Account.query.all()
 
-    # UPDATE
     def update(self):
+        """UPDATE: confirma cambios en la instancia."""
         db.session.commit()
         return self
 
-    # DELETE
     def delete(self):
+        """DELETE: elimina la instancia de la base de datos."""
         db.session.delete(self)
         db.session.commit()
