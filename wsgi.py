1429573ef34549dfd5abea3609d846593cc331ee
Nicole Rodriguez <nicolegonzaleslearn@gmail.com>
2026-07-28 10:00:37 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,4 @@
-[nosetests]
-with-spec=1
-spec-color=1
-with-coverage=1
-cover-erase=1
-cover-package=service
+from service import app
 
-[coverage:run]
-branch = True
-source = service
-
-[coverage:report]
-show_missing = True
-precision = 2
+if __name__ == "__main__":
+    app.run()
